7533e62bd24acb05335b7f16ac6f942cda52205a
Irem Kaya <iremkaya1999@hotmail.com>
2018-10-02 23:09:19 +0300

    added a very big sum

---
+++ algorithms/warmup/a-very-big-sum.py
@@ -0,0 +1,23 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the aVeryBigSum function below.
+def aVeryBigSum(ar):
+    return sum(ar)
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    ar_count = int(input())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = aVeryBigSum(ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
