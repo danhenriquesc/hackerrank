1f53cd2c8a211250b1b63e0ea9e12a5e4eea954e
tal87 <talbsho@gmail.com>
2018-10-03 21:15:33 +0300

    add main method

--- algorithms/warmup/compare_the_triplets.cs
+++ algorithms/warmup/compare_the_triplets.cs
@@ -13,17 +13,26 @@ using System.Text;
 using System;
 
 class Solution {
-  static List<int> compareTriplets(List<int> a, List<int> b) {
-    int[] ret = new int[] { 0, 0 };
-    for(int i = 0; i < a.Count; i++) {
-        if(a[i] > b[i]) {
-            ret[0]++;
+    static List<int> compareTriplets(List<int> a, List<int> b) {
+        int[] ret = new int[] { 0, 0 };
+        for(int i = 0; i < a.Count; i++) {
+            if(a[i] > b[i]) {
+                ret[0]++;
+            }else if(a[i] < b[i]) {
+                ret[1]++;
+            }
         }
-        else if(a[i] < b[i]) {
-            ret[1]++;
-        }
+
+        return ret.ToList();
     }
 
-    return ret.ToList();
-  }
+    static void Main(string[] args) {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+        List<int> a = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(aTemp => Convert.ToInt32(aTemp)).ToList();
+        List<int> b = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(bTemp => Convert.ToInt32(bTemp)).ToList();
+        List<int> result = compareTriplets(a, b);
+        textWriter.WriteLine(String.Join(" ", result));
+        textWriter.Flush();
+        textWriter.Close();
+    }
 }
