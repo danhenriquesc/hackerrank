388f21d683e14b78cefcdce7221b309fb130187d
Srikant <rathoresrikant@gmail.com>
2018-10-05 23:58:22 +0530

    added solution to the coin change problem

---
+++ algorithms/dynamic_programming/coin_change_problem.cpp
@@ -0,0 +1,35 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+
+long getWays(long n, vector<long> c) 
+{
+    long m = c.size();
+    vector <vector <long> > dp(m+1,vector<long> (n+1));
+    for(long i=0; i<=m; i++)
+        dp[i][0] = 1;
+    for(long j=1; j<=n; j++)
+        dp[0][j] = 0;
+    for(long i=1; i<=m ;i++)
+    {
+        for(long j=1; j<=n ; j++)
+        {
+            if(c[i-1] <= j)
+                dp[i][j] = dp[i-1][j] + dp[i][j-c[i-1]];
+            else
+                dp[i][j] = dp[i-1][j];
+        }
+    }
+    return dp[m][n];
+}
+
+int main()
+{
+    long n,m;
+    cin>>n>>m;
+    vector <long> c(m);
+    for(long i=0; i<m; i++)
+        cin>>c[i];
+    long res = getWays(n,c);
+    cout << res;
+    return 0;
+}
