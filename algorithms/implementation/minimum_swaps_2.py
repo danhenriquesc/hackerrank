80cc512e7eba6172e51cc282d17871be03abd15f
Daniel Henrique <daniel.henrique.sc@gmail.com>
2021-10-18 22:56:51 -0300

    Create minimum_swaps_2.py

---
+++ algorithms/implementation/minimum_swaps_2.py
@@ -0,0 +1,21 @@
+import math
+import os
+import random
+import re
+import sys
+
+n = int(input())
+
+arr = list(map(int, input().rstrip().split()))
+
+numSwaps = 0
+i = 0
+while(i < len(arr)-1):
+    if arr[i] != i+1:
+        tmp = arr[i]
+        arr[i], arr[tmp-1] = arr[tmp-1], arr[i]
+        numSwaps += 1
+    else:
+        i += 1
+
+print(numSwaps)
